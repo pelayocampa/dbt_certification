054f939e1f0580b001f920408ab5fd7b5d1abd42
Pelayo Campa González-Nuevo <lattice.fenland_3o@icloud.com>
2026-04-04 09:30:50 +0000

    Refactored and added Financial mart

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,7 +1,7 @@
 with
     customers as (select * from {{ ref("stg_jaffle_shop__customers") }}),
 
-    orders as (select * from {{ ref("stg_jaffle_shop__orders") }}),
+    orders as (select * from {{ ref("fct_orders") }}),
 
     customer_orders as (
 
@@ -10,13 +10,15 @@ with
 
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
 
         from orders
 
         group by 1
 
     ),
+   
 
     final as (
 
@@ -26,12 +28,11 @@ with
             customers.last_name,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            customer_orders.lifetime_value
         from customers
 
         left join customer_orders using (customer_id)
-
     )
 
 select *
